6924c830d6eaa2dcc3b7a1c02dc96f50d247b2d2
nihat417 <akreminihat@gmail.com>
2024-11-10 19:25:07 +0400

    Complated

--- src/app/components/mainMenubar/BasketSidebar.tsx
+++ src/app/components/mainMenubar/BasketSidebar.tsx
@@ -25,6 +25,10 @@
       setSelectedButton(buttonId);
     };
 
+    const closeBasket = () => {
+      setIsBasketOpen(false); 
+    };
+
     const toggleBasket = () => {
       setIsBasketOpen(!isBasketOpen); 
     };
@@ -39,6 +43,7 @@
       }
     };
 
+
     useEffect(() => {
       if (isMenuOpen) 
         document.body.style.overflow = 'hidden';
@@ -52,87 +57,89 @@
 
     return (
       <>
-        <div className={`fixed top-0 left-0 h-full z-20 bg-[#1A1A1A] text-white transition-all duration-300 transform 
-            ${isBasketOpen ? "opacity-100 visible" : "opacity-0 invisible"}
-            ${isBasketOpen ? "lg:w-[35%] w-full" : "w-0"}`}>
-          <div className="p-4 flex justify-center  h-full">
-          <Tabs defaultValue="account" className={`my-[50px] w-[600px] sm:w-[400px] `}>
-              <TabsList className={`grid w-full grid-cols-2 `}>
-                <TabsTrigger value="account" >New</TabsTrigger>
-                <TabsTrigger value="password">Waited</TabsTrigger>
-              </TabsList>
-              <TabsContent value="account">
-                <Card>
-                  <CardContent className="my-[40px] max-h-[200px] space-y-2 overflow-y-auto custom-scrollbar">
-                    {selectedItems.map((item, index) => (
-                      <div key={index} className="flex flex-row justify-between space-y-1">
-                        <div className="flex flex-row">
-                          <Label className="mx-[10px] text-[16px]">{item.quantity}</Label>
-                          <Label className="mx-[10px] font-bold text-[16px]">{item.name}</Label>
+          <div className={`fixed top-0 left-0 h-full z-20 bg-[#1A1A1A] text-white transition-all duration-300 transform 
+              ${isBasketOpen ? "opacity-100 visible" : "opacity-0 invisible"}
+              ${isBasketOpen ? "lg:w-[35%] w-full" : "w-0"}`}>
+            <div className="p-4 flex justify-center  h-full">
+              <button onClick={closeBasket} className="absolute top-4 right-4 text-white text-2xl">
+                &times;   
+              </button>
+              <Tabs defaultValue="account" className={`my-[50px] w-[600px] sm:w-[400px] `}>
+                <TabsList className={`grid w-full grid-cols-2 `}>
+                  <TabsTrigger value="account" >New</TabsTrigger>
+                  <TabsTrigger value="password">Waited</TabsTrigger>
+                </TabsList>
+                <TabsContent value="account">
+                  <Card>
+                    <CardContent className="my-[40px] max-h-[200px] space-y-2 overflow-y-auto custom-scrollbar">
+                      {selectedItems.map((item, index) => (
+                        <div key={index} className="flex flex-row justify-between space-y-1">
+                          <div className="flex flex-row">
+                            <Label className="mx-[10px] text-[16px]">{item.quantity}</Label>
+                            <Label className="mx-[10px] font-bold text-[16px]">{item.name}</Label>
+                          </div>
+                          <Label className="text-[16px]">${item.price * item.quantity}</Label>
+                          <button onClick={() => removeItem(item.name)}>Remove</button>
                         </div>
-                        <Label className="text-[16px]">${item.price * item.quantity}</Label>
-                        <button onClick={() => removeItem(item.name)}>Remove</button>
+                      ))}
+                    </CardContent>
+
+                    <div className="flex flex-row justify-between mx-[25px] my-[40px] p-[5px] border-[#000] border-[1px] border-dotted">
+                      <h4 className="ml-[10px] font-bold text-[18px] text-black">Total</h4>
+                      <h4 className="text-[18px] font-bold text-black">{totalAmount.toFixed(2)}$</h4>
+                    </div>
+
+                    <div className="flex flex-row justify-center gap-6 my-[20px]">
+                      <div>
+                        <button
+                          className={`p-[20px] rounded-[10px] border-[1px] border-[#97D4D4] ${
+                            selectedButton === "cash" ? "bg-[#97D4D4]" : "" 
+                          }`} onClick={() => handleButtonClick("cash")} >
+                          <CiCreditCard1 />
+                        </button>
+                        <p className={`text-center text-[14px] mt-[5px] ${selectedButton === "cash" ? "text-[#97D4D4]" : "" }`}>Cash</p>
                       </div>
-                    ))}
-                  </CardContent>
+                      <div>
+                        <button
+                          className={`p-[20px] rounded-[10px] border-[1px] border-[#97D4D4] ${
+                            selectedButton === "credit" ? "bg-[#97D4D4]" : "" 
+                          }`} onClick={() => handleButtonClick("credit")}>
+                          <FaRegMoneyBillAlt />
+                        </button>
+                        <p className={`text-center text-[14px] mt-[5px] ${selectedButton === "credit" ? "text-[#97D4D4]" : "" }`}>Credit card</p>
+                      </div>
+                      <div>
+                        <button
+                          className={`p-[20px] rounded-[10px] border-[1px] border-[#97D4D4] ${
+                            selectedButton === "customer" ? "bg-[#97D4D4]" : "" 
+                          }`} onClick={() => handleButtonClick("customer")}>
+                          <IoPerson />
+                        </button>
+                        <p className={`text-center text-[14px] mt-[5px] ${selectedButton === "customer" ? "text-[#97D4D4]" : "" }`}>Customer</p>
+                      </div>
+                      <div>
+                        <button
+                          className={`p-[20px] rounded-[10px] border-[1px] border-[#97D4D4] ${
+                            selectedButton === "gift" ? "bg-[#97D4D4]" : "" 
+                          }`} onClick={() => handleButtonClick("gift")}>
+                          <CiGift />
+                        </button>
+                        <p className={`text-center text-[14px] mt-[5px] ${selectedButton === "gift" ? "text-[#97D4D4]" : "" }`}>Gift</p>
+                      </div>
+                    </div>
 
-                  <div className="flex flex-row justify-between mx-[25px] my-[40px] p-[5px] border-[#000] border-[1px] border-dotted">
-                    <h4 className="ml-[10px] font-bold text-[18px] text-black">Total</h4>
-                    <h4 className="text-[18px] font-bold text-black">{totalAmount.toFixed(2)}$</h4>
-                  </div>
-
-                  <div className="flex flex-row justify-center gap-6 my-[20px]">
-                    <div>
-                      <button
-                        className={`p-[20px] rounded-[10px] border-[1px] border-[#97D4D4] ${
-                          selectedButton === "cash" ? "bg-[#97D4D4]" : "" 
-                        }`} onClick={() => handleButtonClick("cash")} >
-                        <CiCreditCard1 />
-                      </button>
-                      <p className={`text-center text-[14px] mt-[5px] ${selectedButton === "cash" ? "text-[#97D4D4]" : "" }`}>Cash</p>
-                    </div>
-                    <div>
-                      <button
-                        className={`p-[20px] rounded-[10px] border-[1px] border-[#97D4D4] ${
-                          selectedButton === "credit" ? "bg-[#97D4D4]" : "" 
-                        }`} onClick={() => handleButtonClick("credit")}>
-                        <FaRegMoneyBillAlt />
-                      </button>
-                      <p className={`text-center text-[14px] mt-[5px] ${selectedButton === "credit" ? "text-[#97D4D4]" : "" }`}>Credit card</p>
-                    </div>
-                    <div>
-                      <button
-                        className={`p-[20px] rounded-[10px] border-[1px] border-[#97D4D4] ${
-                          selectedButton === "customer" ? "bg-[#97D4D4]" : "" 
-                        }`} onClick={() => handleButtonClick("customer")}>
-                        <IoPerson />
-                      </button>
-                      <p className={`text-center text-[14px] mt-[5px] ${selectedButton === "customer" ? "text-[#97D4D4]" : "" }`}>Customer</p>
-                    </div>
-                    <div>
-                      <button
-                        className={`p-[20px] rounded-[10px] border-[1px] border-[#97D4D4] ${
-                          selectedButton === "gift" ? "bg-[#97D4D4]" : "" 
-                        }`} onClick={() => handleButtonClick("gift")}>
-                        <CiGift />
-                      </button>
-                      <p className={`text-center text-[14px] mt-[5px] ${selectedButton === "gift" ? "text-[#97D4D4]" : "" }`}>Gift</p>
-                    </div>
-                  </div>
-
-                  <CardFooter>
-                    <Button className="w-full">complete order</Button>
-                  </CardFooter>
-                </Card>
-              </TabsContent>
-              <TabsContent value="password">
-                <Card>
-                </Card>
-              </TabsContent>
-            </Tabs>
-            
+                    <CardFooter>
+                      <Button className="w-full">complete order</Button>
+                    </CardFooter>
+                  </Card>
+                </TabsContent>
+                <TabsContent value="password">
+                  <Card>
+                  </Card>
+                </TabsContent>
+              </Tabs>
+            </div>
           </div>
-        </div>
 
         <div className={`transition-all duration-300 ${isBasketOpen ? "ml-[35%]" : "ml-0"} flex-grow`}>
           {children}
@@ -146,8 +153,11 @@
       />
 
       {/* Menu */}
-      <div className={`fixed top-0 bottom-0 right-0 rounded-[10px] bg-gray-800 text-white p-6 transition-transform duration-300 transform  
+      <div className={`fixed top-0 bottom-0 right-0 rounded-[10px] bg-[#1A1A1A] text-white p-6 transition-transform duration-300 transform  
        ${isMenuOpen ? "translate-x-0 w-full sm:w-64 md:m-[20px]" : "translate-x-full"} z-20`}>
+        <button onClick={closeMenu} className="absolute top-4 right-4 text-white text-2xl">
+          &times;   
+        </button>
         <h2 className="text-2xl mb-4">Menu</h2>
         <ul>
           <li className="mb-2 cursor-pointer">Home</li>
